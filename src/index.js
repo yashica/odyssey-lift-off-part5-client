a4f9e00387911445db8343b76986d866bddeb5e3
AMBloeckner <magdalena.bloeckner@horizon-alpha.com>
2022-03-22 11:53:57 +0100

    update to production server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://apollo-graphql-lift-off-app.herokuapp.com/', //changed to our own production server uri
   cache: new InMemoryCache(),
 });
 
